943c5684123856e7c3410bce0856750993156955
Giovanni Paiva <paivagio@hotmail.com>
2021-10-19 18:30:10 -0300

    index and js

---
+++ index.js
@@ -0,0 +1,38 @@
+const logo = document.querySelector('#logo');
+const username = document.querySelector('#login a');
+const icon = document.querySelector('#login embed');
+const cart = document.querySelector('#cart-count');
+const cart_num = document.querySelector('#cart-count span');
+storage = window.localStorage;
+
+window.onload = function (e) {
+    var firstTime = JSON.parse(storage.getItem('firstTime'));
+    if (firstTime) {
+        storage.setItem('access', false);
+        storage.setItem('firstTime', false);
+    }
+
+    var access = JSON.parse(storage.getItem('access'));
+    console.log(access);
+    if (access) {
+        var currentUser = JSON.parse(storage.getItem('currentUser'));
+
+        username.textContent = currentUser.username;
+        username.href = '';
+        icon.src = 'images/user_icon_signed.svg';
+
+        let numProducts = currentUser.cart.count;
+        if (numProducts > 0) {
+            cart_num.textContent = numProducts.toString();
+            cart.style.opacity = "1";
+        }
+    }
+};
+
+logo.addEventListener('click', function (e) {
+    storage.setItem('access', false);
+    username.textContent = 'Entrar';
+    username.href = 'pages/login.html';
+    icon.src = 'images/user_icon.svg';
+    cart.style.opacity = "0";
+});
--- scripts/login.js
+++ scripts/login.js
@@ -1,5 +1,29 @@
 const togglePassword = document.querySelector('#togglePassword');
+const submit = document.querySelector('#submitButton');
+const username = document.querySelector('#username');
 const password = document.querySelector('#password');
+const remember = document.querySelector('#rememberMe');
+storage = window.localStorage;
+
+const defaultUser = {
+    "username": 'admin',
+    "password": 'admin123',
+    "name": 'admin',
+    "address": 'admin',
+    "cart": {
+        "count": 2,
+        "products": [
+            {
+                "name": 'Salada c/ salmão',
+                "img": 'salmon_salad.jpg',
+                "category": 'SALADAS',
+                "price": 23.90
+            }
+        ],
+    }
+};
+
+storage.setItem('users', JSON.stringify([defaultUser]));
 
 togglePassword.addEventListener('click', function (e) {
     // toggle the type attribute
@@ -12,3 +36,52 @@ togglePassword.addEventListener('click', function (e) {
     icon.setAttribute('src', src);
 });
 
+username.addEventListener("keyup", ({ key }) => {
+    if (key === "Enter") {
+        login();
+    }
+});
+
+password.addEventListener("keyup", ({ key }) => {
+    if (key === "Enter") {
+        login();
+    }
+});
+
+submit.addEventListener('click', login());
+
+function login(e) {
+    if (!username.value) {
+        username.style.outline = '2px solid red';
+        username.style.setProperty("--c", "red");
+    }
+    else if (!password.value) {
+        password.style.outline = '2px solid red';
+        username.style.outline = 'transparent';
+        username.style.setProperty("--c", "#7A7878");
+        password.style.setProperty("--c", "red");
+    }
+    else {
+        password.style.outline = 'transparent';
+        var database = JSON.parse(storage.getItem('users'));
+        var token = false;
+        var currentUser = null;
+        for (var i = 0; i < database.length; i++) {
+            if (database[i].username === username.value && database[i].password === password.value) {
+                token = true;
+                currentUser = database[i];
+                break;
+            }
+        }
+
+        if (token) {
+            storage.setItem('access', true);
+            storage.setItem('currentUser', JSON.stringify(currentUser));
+            window.location.href = "../index.html";
+        }
+        else {
+            console.log(storage.getItem('access'));
+        }
+    }
+
+};
