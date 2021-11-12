42a223580c2e2d8ea1af025f3f8afa4d3ae0482b
Giovanni Paiva <paivagio@hotmail.com>
2021-11-12 13:54:27 -0300

    Update checkout javascript

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,7 +1,37 @@
 storage = window.localStorage;
+const exit = document.querySelector('#exitIcon');
+const profileIcon = document.querySelector('#profileIcon');
+const usernameDecoration = document.querySelector('.status-acount');
+const username = document.querySelector('.status-acount span');
+const resume = document.querySelector('.order-summary-value span');
+const totalAmount = document.querySelector('.order-total-value span');
 
 window.onload = () => {
   loadCart();
+
+  var access = JSON.parse(storage.getItem('access'));
+  var user = access ? JSON.parse(storage.getItem('currentUser')) : JSON.parse(storage.getItem('nonLoggedUser'));
+  //console.log(access);
+  if (access) {
+    profileIcon.src = '../images/user_icon_signed.svg';
+    exit.style.display = 'block';
+    profileIcon.parentElement.href = '../index.html';
+    username.textContent = user.name.toUpperCase();
+    username.style.color = '#FFFF';
+    usernameDecoration.style.backgroundColor = '#009C8E';
+  }
+  var total = 0;
+  for (const product of user.cart.products) {
+    var cleanPrice = product.price.replace(/R\$ |R\$/g, '');
+    total += parseFloat(cleanPrice);
+  }
+  total = Number(total.toFixed(2));
+  total = total.toString();
+  if (total.length < 5) {
+    total = total + '0';
+  }
+  resume.textContent = 'R$ ' + total;
+  totalAmount.textContent = 'R$ ' + total;
 }
 
 function loadCart() {
@@ -15,28 +45,45 @@ function loadCart() {
   //       "price": 23.90
   //   }
   // ],
-
+  var item = ""
   for (const product of cart.products) {
-    var item = ""
     item += '<div class="order-item">'
     item += '<div>'
-    item += '<img src="../images/products/'+ product.img +'.jpg" alt="salmon_salad">'
+    item += '<img src="../images/products/' + product.img + '" alt="">'
     item += '</div>'
     item += '<div class="order-info">'
-    item += '<p><b>'+ product.name +'</b></p>'
-    item += '<p>Categoria: <b>'+ product.category +'</b></p>'
+    item += '<p><b>' + product.name + '</b></p>'
+    item += '<p>Categoria: <b>' + product.category + '</b></p>'
     item += '<p>Peso: <b>250g</b></p>'
     item += '</div>'
 
     item += '<div class="order-price">'
-    item += '<span>R$ '+ product.price +'</span>'
+    item += '<span>' + product.price + '</span>'
     item += '</div>'
 
     item += '<div class="order-amount">'
     item += '<p>Quantidade:</p>'
-    item += '<input type="number"/>'
+    item += '<input type="number" value="1"/>'
+    item += '</div>'
     item += '</div>'
   }
 
-  document.querySelector('.order-content').innerHTML += item
+  document.querySelector('.order-content').innerHTML = item
 }
+
+exit.addEventListener('click', function (e) {
+  storage.setItem('access', false);
+
+  const nonLoggedUser = {
+    "username": 'offline',
+    "password": 'offline',
+    "name": 'offline',
+    "address": 'offline',
+    "cart": {
+      "count": 0,
+      "products": [],
+    }
+  }
+  storage.setItem('nonLoggedUser', JSON.stringify(nonLoggedUser));
+  window.location = "../index.html";
+});
--- scripts/checkoutFunctions.js
+++ scripts/checkoutFunctions.js
@@ -1,8 +1,49 @@
 const paymentInput = document.getElementsByName("payment-selected");
 const divCardRegistration = document.querySelector('.card-registration');
-
 const cardInputs = document.querySelector('.card-registration form');
+storage = window.localStorage;
+const exit = document.querySelector('#exitIcon');
+const profileIcon = document.querySelector('#profileIcon');
+const usernameDecoration = document.querySelector('.status-acount');
+const username = document.querySelector('.status-acount span');
+const resume = document.querySelector('.order-summary-value span');
+const totalAmount = document.querySelector('.order-total-value span');
+const cart = document.querySelector('#cart-count');
+const cart_num = document.querySelector('#cart-count span');
 
+window.onload = () => {
+
+  var access = JSON.parse(storage.getItem('access'));
+  var user = access ? JSON.parse(storage.getItem('currentUser')) : JSON.parse(storage.getItem('nonLoggedUser'));
+  //console.log(access);
+  if (access) {
+    profileIcon.src = '../images/user_icon_signed.svg';
+    exit.style.display = 'block';
+    profileIcon.parentElement.href = '../index.html';
+    username.textContent = user.name.toUpperCase();
+    username.style.color = '#FFFF';
+    usernameDecoration.style.backgroundColor = '#009C8E';
+  }
+  var total = 0;
+  for (const product of user.cart.products) {
+    var cleanPrice = product.price.replace(/R\$ |R\$/g, '');
+    total += parseFloat(cleanPrice);
+  }
+  total = Number(total.toFixed(2));
+  total = total.toString();
+  if (total.length < 5) {
+    total = total + '0';
+  }
+  resume.textContent = 'R$ ' + total;
+  totalAmount.textContent = 'R$ ' + total;
+
+  var numProducts = user.cart.count;
+  //console.log(numProducts)
+  if (numProducts > 0) {
+    cart_num.textContent = numProducts.toString();
+    cart.style.opacity = "1";
+  }
+}
 // console.log(cardInput[1].checked);
 
 function showCardRegistration() {
@@ -24,13 +65,31 @@ function checkoutValidate() {
   } else if (paymentInput[1].checked === true) {
     var cardInfo = cardInputs.elements;
     for (const input of cardInfo) {
-      console.log(input.value);
+      //console.log(input.value);
       if (input.value === "") {
         alert("Favor preencher os dados do cartão")
         return;
       }
     }
+    window.location.href = "checkoutConfirmation.html"
   } else {
     window.location.href = "checkoutConfirmation.html"
   }
 }
+
+exit.addEventListener('click', function (e) {
+  storage.setItem('access', false);
+
+  const nonLoggedUser = {
+    "username": 'offline',
+    "password": 'offline',
+    "name": 'offline',
+    "address": 'offline',
+    "cart": {
+      "count": 0,
+      "products": [],
+    }
+  }
+  storage.setItem('nonLoggedUser', JSON.stringify(nonLoggedUser));
+  window.location = "../index.html";
+});
